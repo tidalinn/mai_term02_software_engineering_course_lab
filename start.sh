42ac9825f7f304deaa5e5b9b3d3e1ad2e047a9b5
Dmitriy Dzyuba <ddzuba@yandex.ru>
2021-10-16 17:29:54 +0000

    fixes

--- start.sh
+++ start.sh
@@ -1,2 +1,2 @@
-sudo ./CMakeFiles/hl_mai_lab_01 --host=127.0.0.1 --port=3306 --login=test --password=pzjqUkMnc7vfNHET --database=sql_test
+sudo ./build/hl_mai_lab_01 --host=localhost --port=3306 --login=stud --password=stud --database=stud
 
--- web_server/handlers/author_handler.h
+++ web_server/handlers/author_handler.h
@@ -118,7 +118,7 @@ public:
             }
             catch (...)
             {
-                ostr << "{ \"result\": false , \"reason\": \"not gound\" }";
+                ostr << "{ \"result\": false , \"reason\": \"not found\" }";
                 return;
             }
         }
